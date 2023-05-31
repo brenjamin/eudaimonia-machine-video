69a2f95386c6990f2a2f5ebb6e23ca908cbe2b9b
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-05-31 10:35:24 +0100

    aa

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,19 +1,7 @@
 import {Sequence, useVideoConfig} from 'remotion';
-import {Scene10Salon} from './Scene10Salon';
-import {Scene11Library} from './Scene11Library';
-import {Scene12Office} from './Scene12Office';
-import {Scene13DeepWorkChambers} from './Scene13DeepWorkChambers';
 import {Scene1DeepWorkIsRare} from './Scene1DeepWorkIsRare';
 import {Scene2DeepWorkIsHard} from './Scene2DeepWorkIsHard';
-import {Scene3EudaimoniaMachine} from './Scene3EudaimoniaMachine';
-import {Scene4Eudaimonia} from './Scene4Eudaimonia';
-import {Scene4aAchieveEudaimonia} from './Scene4aAchieveEudaimonia';
-import {Scene5DavidDewane} from './Scene5DavidDewane';
-import {Scene6Building} from './Scene6Building';
-import {Scene7OpenOffices} from './Scene7OpenOffices';
-import {Scene8MachinePurpose} from './Scene8MachinePurpose';
-import {Scene9Gallery} from './Scene9Gallery';
-import {Scene14EudaimoniaOutro} from './Scene14EudaimoniaOutro';
+
 import {Scene15Outro} from './Scene15Outro';
 import {Narration} from './Narration';
 import {OutroMusic} from './OutroMusic';
@@ -22,10 +10,10 @@ import {Scene1aKnowledgeWorkers} from './Scene1aKnowledgeWorkers';
 export const Main: React.FC = () => {
 	return (
 		<>
-			<Sequence from={0} durationInFrames={10350}>
+			<Sequence durationInFrames={10350}>
 				<Narration />
 			</Sequence>
-			<Sequence from={0} durationInFrames={540}>
+			<Sequence durationInFrames={540}>
 				<Scene1DeepWorkIsRare />
 			</Sequence>
 			<Sequence from={540} durationInFrames={760}>
@@ -34,45 +22,8 @@ export const Main: React.FC = () => {
 			<Sequence from={1300} durationInFrames={210}>
 				<Scene2DeepWorkIsHard />
 			</Sequence>
-			<Sequence from={1510} durationInFrames={270}>
-				<Scene3EudaimoniaMachine />
-			</Sequence>
-			<Sequence from={1780} durationInFrames={760}>
-				<Scene4Eudaimonia />
-			</Sequence>
-			<Sequence from={2540} durationInFrames={940}>
-				<Scene4aAchieveEudaimonia />
-			</Sequence>
-			<Sequence from={3480} durationInFrames={620}>
-				<Scene5DavidDewane />
-			</Sequence>
-			<Sequence from={4100} durationInFrames={1670}>
-				<Scene6Building />
-			</Sequence>
-			<Sequence from={5705} durationInFrames={390}>
-				<Scene7OpenOffices />
-			</Sequence>
-			<Sequence from={6095} durationInFrames={380}>
-				<Scene8MachinePurpose />
-			</Sequence>
-			<Sequence from={6475} durationInFrames={360}>
-				<Scene9Gallery />
-			</Sequence>
-			<Sequence from={6835} durationInFrames={405}>
-				<Scene10Salon />
-			</Sequence>
-			<Sequence from={7240} durationInFrames={455}>
-				<Scene11Library />
-			</Sequence>
-			<Sequence from={7695} durationInFrames={230}>
-				<Scene12Office />
-			</Sequence>
-			<Sequence from={7925} durationInFrames={720}>
-				<Scene13DeepWorkChambers />
-			</Sequence>
-			<Sequence from={8645} durationInFrames={1020}>
-				<Scene14EudaimoniaOutro />
-			</Sequence>
+			
+			
 			<Sequence from={8920} durationInFrames={1430}>
 				<OutroMusic />
 			</Sequence>
--- src/Root.tsx
+++ src/Root.tsx
@@ -3,18 +3,7 @@ import {Main} from './Main';
 import {Scene1DeepWorkIsRare} from './Scene1DeepWorkIsRare';
 import {Scene1aKnowledgeWorkers} from './Scene1aKnowledgeWorkers';
 import {Scene2DeepWorkIsHard} from './Scene2DeepWorkIsHard';
-import {Scene3EudaimoniaMachine} from './Scene3EudaimoniaMachine';
-import {Scene4Eudaimonia} from './Scene4Eudaimonia';
-import {Scene4aAchieveEudaimonia} from './Scene4aAchieveEudaimonia';
-import {Scene5DavidDewane} from './Scene5DavidDewane';
-import {Scene6Building} from './Scene6Building';
-import {Scene7OpenOffices} from './Scene7OpenOffices';
-import {Scene8MachinePurpose} from './Scene8MachinePurpose';
-import {Scene9Gallery} from './Scene9Gallery';
-import {Scene10Salon} from './Scene10Salon';
-import {Scene11Library} from './Scene11Library';
-import {Scene12Office} from './Scene12Office';
-import {Scene13DeepWorkChambers} from './Scene13DeepWorkChambers';
+
 import {Scene14EudaimoniaOutro} from './Scene14EudaimoniaOutro';
 import {Scene15Outro} from './Scene15Outro';
 import {Narration} from './Narration';
@@ -47,102 +36,7 @@ export const RemotionRoot: React.FC = () => {
 				width={1920}
 				height={1080}
 			/>
-			<Composition
-				id="Scene3EudaimoniaMachine"
-				component={Scene3EudaimoniaMachine}
-				durationInFrames={180}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene4Eudaimonia"
-				component={Scene4Eudaimonia}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene4aAchieveEudaimonia"
-				component={Scene4aAchieveEudaimonia}
-				durationInFrames={940}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene5DavidDewane"
-				component={Scene5DavidDewane}
-				durationInFrames={620}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene6Building"
-				component={Scene6Building}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene7OpenOffices"
-				component={Scene7OpenOffices}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene8MachinePurpose"
-				component={Scene8MachinePurpose}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene9Gallery"
-				component={Scene9Gallery}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene10Salon"
-				component={Scene10Salon}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene11Library"
-				component={Scene11Library}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene12Office"
-				component={Scene12Office}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Scene13DeepWorkChambers"
-				component={Scene13DeepWorkChambers}
-				durationInFrames={240}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
+
 			<Composition
 				id="Scene14EudaimoniaOutro"
 				component={Scene14EudaimoniaOutro}
@@ -162,11 +56,12 @@ export const RemotionRoot: React.FC = () => {
 			<Composition
 				id="Narration"
 				component={Narration}
-				durationInFrames={2370 + 720 + 930}
+				durationInFrames={4020}
 				fps={30}
 				width={1920}
 				height={1080}
-			></Composition>
+			/>
+			
 			<Composition
 				id="Main"
 				component={Main}
